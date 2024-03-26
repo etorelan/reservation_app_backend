868ce2b74c1e1a178b1e1013c93a28afabb1b799
etorelan <etorelan@protonmail.com>
2024-03-26 21:24:24 +0100

    m

--- netflix_clone_backend/settings.py
+++ netflix_clone_backend/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 24 
 }
 if not DEBUG:
-    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS_DEPLOY").split(",")
+    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS_DEPLOY")
     CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS_DEPLOY").split(",")
     CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS_DEPLOY").split(",")
     DATABASES = {
